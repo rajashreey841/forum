68d48389c5cf139645c45ad64830a402b5823cd0
Ravi Kumar Reddy K <ravikumark815@gmail.com>
2021-01-05 19:55:39 +0530

    Solving Post a Question issue

--- code/pages/question-function.php
+++ code/pages/question-function.php
@@ -5,17 +5,30 @@ date_default_timezone_set("Asia/Taipei");
 $datetime = date("Y-m-d h:i:sa");
 
 extract($_POST);
-$sql = "INSERT INTO tblpost(post_id,title,content, datetime,cat_id,user_Id) VALUES ('$title','$content','$category','$datetime','$user')";
+echo '<script type="text/javascript">alert("'.$category.'");</script>';
+echo '<script type="text/javascript">alert("'.gettype($category).'");</script>';
+if ($category === '1') {
+        $category_id = 1;
+}
+else if ($category === '2') {
+        $category_id = 2;
+}
+else if($category === '6') {
+        $category_id = 6;
+}
+echo '<script type="text/javascript">alert("'.$category_id.'");</script>';
+$sql = "INSERT INTO `tblpost`(`title`, `content`, `datetime`, `cat_id`, `user_Id`) VALUES ('$title','$content', '$datetime', $category_id, 'raji')";
+echo '<script type="text/javascript">alert("'.$sql.'");</script>';
 $res = mysqli_query($con, $sql);
 
 if ($res == true) {
         echo '<script language="javascript">';
-        echo 'alert("Post Successfully")';
+        echo 'alert("Posted Successfully")';
         echo '</script>';
         echo '<meta http-equiv="refresh" content="0;url=home.php" />';
 } else {
         echo '<script language="javascript">';
-        echo 'alert("Post unSuccessfully")';
+        echo 'alert("Post Unsuccessful")';
         echo '</script>';
         echo '<meta http-equiv="refresh" content="0;url=home.php" />';
 }
